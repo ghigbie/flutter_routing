0e02a7223dba9331631047e3a14703a2579310fc
ghigbie <gehigbie@gmail.com>
2019-06-04 21:35:56 -0600

    Edit settings screen

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class SettingsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return null;
+    return Container(
+      color: Colors.purple[400],
+      child: Center(
+        child: RaisedButton(
+          child: Text('Leave Settings Screen'),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        )
+      )
+    );
   }
 }
