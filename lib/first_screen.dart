f4fb8d31cf0a007e2ac6a10bc9729e33bcd37b91
ghigbie <gehigbie@gmail.com>
2019-06-04 20:26:44 -0600

    Add first class

---
+++ lib/first_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class _FirstScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext contex){
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: RaisedButton(
+          child: Text('Go To Second Screen'),
+          onPressed: (){
+            //Navigator.push(context, MaterialPageRoute(builder: (context) => _SecondScreen())
+          }
+        )
+      )
+    );
+  }
+}
