5fb80fc9f80df56440dccea7a38fb8f6cfb0b930
ghigbie <gehigbie@gmail.com>
2019-06-04 20:48:09 -0600

    Add third screen

--- lib/third_screen.dart
+++ lib/third_screen.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'first_screen.dart';
 
-class SecondScreen extends StatelessWidget{
-  @override 
+class ThirdScreen extends StatelessWidget{
+  @override
   Widget build(BuildContext context){
     return Container(
-      color: Colors.blueGrey[400],
+      color: Colors.pink[400],
       child: Center(
         child: RaisedButton(
           child: Text('Go Back'),
-          onPressed: () {
+          onPressed: (){
 
           }
         )
