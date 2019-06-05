d1d8885a260b5e30d8eb04935d0c71b51078bd96
ghigbie <gehigbie@gmail.com>
2019-06-04 21:22:00 -0600

    Reconfigure first screen

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -4,19 +4,23 @@ import 'second_screen.dart';
 class FirstScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: RaisedButton(
-          child: Text('Go To Second Screen'),
-          onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => SecondScreen()
-              )
-            );
-          }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Routing')),
+      body: Container(
+        color: Colors.blueGrey[400],
+        child: Center(
+          child: RaisedButton(
+            child: Text('Go To Second Screen'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondScreen()
+                )
+              );
+            }
+          )
         )
       )
     );
