68d8f16e44a1c99911c37f0f9e727918cd2d059b
ghigbie <gehigbie@gmail.com>
2019-06-04 21:36:31 -0600

    Change screen colors

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -5,7 +5,7 @@ class SecondScreen extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
     return Container(
-      color: Colors.blueGrey[400],
+      color: Colors.pink[400],
       child: Center(
         child: RaisedButton(
           child: Text('Go Back'),
